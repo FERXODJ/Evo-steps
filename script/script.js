d66bd4de8b09efd55a406e2dd003d0c2e279e3d5
FERXODJ <joseduvelyhuiza@gmail.com>
2025-05-10 00:31:02 -0400

    Se arreglo todo el Inicio

---
+++ script/script.js
@@ -0,0 +1,12 @@
+document.getElementById('boton-carga').addEventListener('click', function () {
+    const overlay = document.getElementById('cargando-overlay');
+
+    // Muestra la animación de carga
+    overlay.classList.add('activo');
+
+    // Redirige al usuario después de 5 segundos
+    setTimeout(() => {
+        overlay.classList.remove('activo'); // Oculta la animación (opcional)
+        window.location.href = 'home.html'; // Cambia 'home.html' por la URL de tu página de inicio
+    }, 5000);
+});
