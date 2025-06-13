82e7e3d14657d10b51c67d0b81fb3b8ffcaf29fa
FERXODJ <joseduvelyhuiza@gmail.com>
2025-06-13 07:13:51 -0400

    Haciend cambios Finales

--- script/videos.js
+++ script/videos.js
@@ -1,41 +1,48 @@
 const videos = [
+
     {
         modulo: "MÓDULO 1",
+        subtitulo: "Binevenid@ - Define tu porqué",
+        src: "https://www.youtube.com/embed/-ylLzZ2GeLc"
+    },
+
+    {
+        modulo: "MÓDULO 2",
         subtitulo: "Aprende a Edificar",
         src: "https://www.youtube.com/embed/cVyF-OUiBz4"
     },
     {
-        modulo: "MÓDULO 2",
+        modulo: "MÓDULO 3",
         subtitulo: "¿Cómo hacer una lista?",
         src: "https://www.youtube.com/embed/X1HzsWaK4cI"
     },
 
     {
-        modulo: "MÓDULO 3",
+        modulo: "MÓDULO 4",
         subtitulo: "¿Cómo Prospectar?",
         src: "https://www.youtube.com/embed/MR5OyHY6xjo"
     },
 
     {
-        modulo: "MÓDULO 4",
+        modulo: "MÓDULO 5",
         subtitulo: "¿Cómo hacer una invitación irresistible?",
         src: "https://www.youtube.com/embed/WoqvlnDp7_Y"
     },
 
     {
-        modulo: "MÓDULO 5",
+        modulo: "MÓDULO 6",
         subtitulo: "Tipos de presentaciones",
         src: "https://www.youtube.com/embed/UI3E01UQXPU"
     },
 
     {
-        modulo: "MÓDULO 6",
+        modulo: "MÓDULO 7",
         subtitulo: "¿Cómo hacer un cierre?",
         src: "https://www.youtube.com/embed/diYRhA2P92Q?start=1"
     },
 
     {
-        modulo: "MÓDULO 7",
+        modulo: "MÓDULO 8",
         subtitulo: "¿Cómo hacer un seguimiento?",
         src: "https://www.youtube.com/embed/kqxD6Y6UgcA"
     },
