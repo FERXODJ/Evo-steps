d368289132ddd5da4240a25f4ff1da45fb4db207
FERXODJ <joseduvelyhuiza@gmail.com>
2025-06-17 13:45:49 -0400

    Haciendo cambias en el side bar

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 
+// Función para alternar el sidebar
 function toggleMobileSidebar() {
     const sidebar = document.getElementById('mobile-sidebar');
     sidebar.classList.toggle('active'); // Alterna la clase 'active'
@@ -16,6 +17,18 @@ document.getElementById('boton-carga').addEventListener('click', function () {
         overlay.classList.remove('activo'); // Oculta la animación (opcional)
         window.location.href = 'home.html'; // Cambia 'home.html' por la URL de tu página de inicio
     }, 5000);
+});
+
+// Resalta el enlace activo en el sidebar
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.mobile-sidebar-menu a');
+    const currentUrl = window.location.pathname;
+
+    links.forEach(link => {
+        if (link.getAttribute('href') === currentUrl) {
+            link.classList.add('active'); // Agrega la clase 'active' al enlace correspondiente
+        }
+    });
 });
 
 
