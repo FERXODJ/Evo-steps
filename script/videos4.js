5890dc5f592bad8f7b9b2041e0df6fca23c1f18b
FERXODJ <joseduvelyhuiza@gmail.com>
2025-08-07 09:11:25 -0400

    Haciendo cambios en Sinteticos

--- script/videos4.js
+++ script/videos4.js
@@ -1,7 +1,12 @@
 const videos = [
     {
         modulo: "Crear tu cuenta demo de Deriv y vincular en Mt5",
-        src: "https://www.youtube.com/embed/tc&t=1s"
+        src: "https://www.youtube.com/embed/a4oWnTo_-tc?start=1"
+    },
+
+    {
+        modulo: "Crear tu cuenta Real de Deriv y vincular en Mt5 y Depositar",
+        src: "https://www.youtube.com/embed/2kf-i3m9RKg"
     },
     // Agrega más objetos aquí para más módulos/videos
 ];
